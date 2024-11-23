449be166cd2934ddfefff615013e31097c6a79f0
KUBIXQAZ <kubajozwik20027@gmail.com>
2024-11-23 16:09:03 +0100

    api

---
+++ API/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private string connectionString;
+
+        public LoginController(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection")!;
+        }
+
+        [HttpGet("login/{username}/{password}")]
+        public IActionResult Login(string username, string password)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string sql = "SELECT * FROM users WHERE Username = @Username && Password = @Password;";
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.Parameters.AddWithValue("@Password", password);
+
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if(reader.HasRows)
+                            {
+                                return Ok(true);
+                            }
+                            else
+                            {
+                                return NotFound(false);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return BadRequest("Internal server error!");
+            }
+        }
+    }
+}
---
+++ API/Controllers/OperationsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OperationsController : Controller
+    {
+        [HttpGet("add/{first}/{second}")]
+        public IActionResult Add(string first, string second)
+        {
+            try
+            {
+                float a = float.Parse(first);
+                float b = float.Parse(second);
+
+                return Ok(a + b);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return BadRequest("Internal server error!");
+            }
+        }
+
+        [HttpGet("subtract/{first}/{second}")]
+        public IActionResult Subtract(string first, string second)
+        {
+            try
+            {
+                float a = float.Parse(first);
+                float b = float.Parse(second);
+
+                return Ok(a - b);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return BadRequest("Internal server error!");
+            }
+        }
+
+        [HttpGet("multiply/{first}/{second}")]
+        public IActionResult Multiply(string first, string second)
+        {
+            try
+            {
+                float a = float.Parse(first);
+                float b = float.Parse(second);
+
+                return Ok(a * b);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return BadRequest("Internal server error!");
+            }
+        }
+
+        [HttpGet("devide/{first}/{second}")]
+        public IActionResult Devide(string first, string second)
+        {
+            try
+            {
+                float a = float.Parse(first);
+                float b = float.Parse(second);
+
+                if (b == 0) return BadRequest("You can't devide by 0.");
+
+                return Ok(a / b);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return BadRequest("Internal server error!");
+            }
+        }
+    }
+}
